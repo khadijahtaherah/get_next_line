88206e106a7109db51dce81d832ed94fbbe8c5cd
Khadijah Taherah <59830938+khadijahtaherah@users.noreply.github.com>
2025-02-07 23:39:22 +0800

    Initial commit

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* *************************************************************************** */
+/*                                                                             */
+/*                                                         :::      ::::::::   */
+/*   get_next_line.h                                     :+:      :+:    :+:   */
+/*                                                     +:+ +:+         +:+     */
+/*   By: skhairul <skhairul@student.42kl.edu.my      +#+  +:+       +#+        */
+/*                                                 +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 23:30:39 by skhairul           #+#    #+#             */
+/*   Updated: 2025/02/07 23:31:56 by skhairul          ###   ########.fr       */
+/*                                                                             */
+/* *************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+#endif
+
