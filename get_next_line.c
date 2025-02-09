1b7632a7d18cc16fb40344ff8ce86cc41fda7a43
Khadijah Taherah <59830938+khadijahtaherah@users.noreply.github.com>
2025-02-09 23:38:46 +0800

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,13 +1,98 @@
-/* *************************************************************************** */
-/*                                                                             */
-/*                                                         :::      ::::::::   */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                     +:+ +:+         +:+     */
-/*   By: skhairul <skhairul@student.42kl.edu.my      +#+  +:+       +#+        */
-/*                                                 +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/07 23:29:45 by skhairul           #+#    #+#             */
-/*   Updated: 2025/02/07 23:32:44 by skhairul         ###   ########.fr       */
-/*                                                                             */
-/* *************************************************************************** */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skhairul <skhairul@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/09 11:09:30 by skhairul          #+#    #+#             */
+/*   Updated: 2025/02/09 11:09:36 by skhairul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "get_next_line.h"
 
+char	*ft_readed_line(char *start)
+{
+	int		i;
+	char	*line;
+
+	if (!start || !start[0])
+		return (NULL);
+	i = 0;
+	while (start[i] && start[i] != '\n')
+		i++;
+	if (start[i] == '\n')
+		i++;
+	line = (char *)malloc(1 + i * sizeof(char));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (start[i] && start[i] != '\n')
+	{
+		line[i] = start[i];
+		i++;
+	}
+	if (start[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_move_start(char	*start)
+{
+	char	*new_buff;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (start[i] && start[i] != '\n')
+		i++;
+	if (start[i] == '\0')
+	{
+		free(start);
+		return (NULL);
+	}
+	i += (start[i] == '\n');
+	new_buff = (char *)malloc(1 + ft_strlen(start) - i);
+	if (!new_buff)
+		return (NULL);
+	j = 0;
+	while (start[i + j])
+	{
+		new_buff[j] = start[i + j];
+		j++;
+	}
+	new_buff[j] = '\0';
+	free(start);
+	return (new_buff);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*tmp;
+	int			byte_read;
+	static char	*start_str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	byte_read = 1;
+	tmp = (char *)malloc(1 + BUFFER_SIZE * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	while (!(ft_strchr(start_str, '\n')) && byte_read != 0)
+	{
+		byte_read = read(fd, tmp, BUFFER_SIZE);
+		if (byte_read == -1)
+		{
+			free(tmp);
+			return (NULL);
+		}
+		tmp[byte_read] = '\0';
+		start_str = ft_strjoin(start_str, tmp);
+	}
+	free(tmp);
+	tmp = ft_readed_line(start_str);
+	start_str = ft_move_start(start_str);
+	return (tmp);
+}
